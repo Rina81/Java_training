6e1a255b93815be441f063ccf361f073c09ef0d7
Katsiaryna Mytsyk <kate.mytsyk@gmail.com>
2021-07-24 16:29:08 +0200

    Task2 - print out doesn't work

---
+++ sandbox/src/main/java/Point/Point.java
@@ -0,0 +1,10 @@
+package Point;
+
+public class Point {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//public static double distance(Point p1, Point p2)
+	}
+
+}
